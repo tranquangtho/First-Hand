3eb02cc4937453f443e7993bc98c5b6dc92e93af
tranquangtho <tranquangtho61220@gmail.com>
2024-10-21 22:37:58 +0700

    12121

--- App.js
+++ App.js
@@ -8,7 +8,7 @@ import Icon from 'react-native-vector-icons/FontAwesome'
 const App=() =>{
   return (
     <SafeAreaView>
-      {/tgtgtgtgtgjkhjghẻgfwyhdhdhe}
+      {/tgtgtgtgtgjkhjghẻgfwyhdhdheejhrfgrsdhgwhyv}
       <DetailProduct/>
       <ProductList />
     </SafeAreaView>
